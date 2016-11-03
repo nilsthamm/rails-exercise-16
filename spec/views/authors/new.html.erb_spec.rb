ca4fcd275d73ee788fcfbd9110b20c558f1fa049
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-03 17:30:14 +0100

    Failing test for #7

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -1,5 +1,11 @@
 # require 'rails_helper'
 
-# RSpec.describe "authors/new.html.erb", type: :view do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe "authors/new" do
+	it "displays fields for first name, last name and homepage" do
+		render :template => "authors/new.html.erb"
+
+		expect(rendered).to match /first_name/
+		expect(rendered).to match /last_name/
+		expect(rendered).to match /homepage/
+	end
+end
