62ca5e32bfc582a15807a097ae8588cb4f309720
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-04 11:20:49 +0100

    Failing test for #12

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+	before :each do #do this before each test
+		@alan_turing= Author.create(first_name: 'Alan', last_name: 'Turing')
+	end
+
+	describe "methode name" do
+		it "should return 'first_name last_name' of the author" do
+			expect(@alan_turing.name).to eq "Alan Turing"
+		end
+	end
 end
