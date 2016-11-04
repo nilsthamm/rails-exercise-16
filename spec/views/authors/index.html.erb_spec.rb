36177416e6c2915e443753818765eb7230dca8a8
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-04 18:11:31 +0100

    Failing test for #27

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "authors/index" do
+	before :each do
+    create(:author)
+  end
+
 	it "has a link to authors/new" do
 		assign(:authors, Author.all)
 		render :template => "authors/index.html.erb"
@@ -9,10 +13,16 @@ describe "authors/index" do
 	end
 
 	it "has a link to an authors page" do
-    Author.create(first_name: 'Alan', last_name: 'Turing', homepage:"http://wikipedia.de/alan_turing")
 		assign(:authors, Author.all)
 		render :template => "authors/index.html.erb"
 
 		expect(rendered).to have_selector 'a', :text => 'Show'
+	end
+
+	it "has a link to an authors edit page" do
+		assign(:authors, Author.all)
+		render :template => "authors/index.html.erb"
+
+		expect(rendered).to have_selector 'a', :text => 'Edit'
 	end
 end
