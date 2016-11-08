dd63f45404205aef6c8b0d1b0805c3a9bad44279
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-08 18:17:59 +0100

    refactoring [ci skip]

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,14 +1,8 @@
 class Author < ActiveRecord::Base
 	has_and_belongs_to_many :papers
 	validates :last_name, :presence => true
-  #after_initialize :default_values
-
 
 	def name
 		"#{first_name} #{last_name}"
 	end	
-	# private
- #    def default_values
- #      self.papers ||= Array.new()
- #    end
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,10 +2,5 @@ class Paper < ActiveRecord::Base
 	has_and_belongs_to_many :authors
 	validates :title, :venue, :year, :presence => true
   validates :year, numericality: { only_integer: true } 
-  #after_initialize :default_values
-
-  # private
-  #   def default_values
-  #     self.authors ||= Array.new()
-  #   end
+  
 end
