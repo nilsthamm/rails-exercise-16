b1fea9c4e0a9a935ada7b128bb4ae15c3af73d1d
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-08 14:03:27 +0100

    Closes #35

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -2,4 +2,17 @@ class PapersController < ApplicationController
   def new
   	@paper = Paper.new
   end
+
+  def create
+	  @paper = Paper.new(paper_params)
+	 
+	  @paper.save
+
+  	render 'new'
+	end
+
+	private
+	  def paper_params
+	    params.require(:paper).permit(:title, :venue, :year)
+	  end
 end
