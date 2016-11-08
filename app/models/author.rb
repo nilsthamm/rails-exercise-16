d9fec4670f32c840a4f9786d75036979bf8b8917
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-08 17:41:10 +0100

    Closes #42

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,14 @@
 class Author < ActiveRecord::Base
+	attr_accessor :papers
 	validates :last_name, :presence => true
+  after_initialize :default_values
+
+
 	def name
 		"#{first_name} #{last_name}"
 	end	
+	private
+    def default_values
+      self.papers ||= Array.new()
+    end
 end
