f0c2a6705f3e705a6cca62582c4bc36d3c202c57
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-08 17:21:22 +0100

    Closes #41

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,11 @@
 class Paper < ActiveRecord::Base
+	attr_accessor :authors
 	validates :title, :venue, :year, :presence => true
-  validates :year, numericality: { only_integer: true }
+  validates :year, numericality: { only_integer: true } 
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.authors ||= Array.new()
+    end
 end
