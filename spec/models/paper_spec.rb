155a12d199ae4e635d83fdcb0456c8e81478762a
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-08 13:54:20 +0100

    Failing test for #34

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
 	describe "validation" do
 		it "paper without title shouldn't be valid" do
-			Paper.new(:venue => "mind 49: 433-460", :year => "1950").should_not be_valid
+			Paper.new(:venue => "mind 49: 433-460", :year => 1950).should_not be_valid
 		end
 
 		it "paper without title shouldn't be valid" do
-			Paper.new(:title => "computing machinery and intelligence", :year => "1950").should_not be_valid
+			Paper.new(:title => "computing machinery and intelligence", :year => 1950).should_not be_valid
 		end
 
 		it "paper without year shouldn't be valid" do
 			Paper.new(:title => "computing machinery and intelligence", :venue => "mind 49: 433-460").should_not be_valid
+		end
+
+		it "paper with non-integer year shouldn't be valid" do
+			Paper.new(:title => "computing machinery and intelligence", :venue => "mind 49: 433-460", :year => "nineteen-fifty").should_not be_valid
 		end
 	end
 end
