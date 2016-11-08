c19b19b3e5833375e0512a4e5eb8fe1fdf80522a
Nils Thamm <nils.thamm@student.hpi.de>
2016-11-08 11:37:31 +0100

    Failing test for #32

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
 	describe "validation" do
-		it "author without lastname shouldn't be valid" do
+		it "paper without title shouldn't be valid" do
 			Paper.new(:venue => "mind 49: 433-460", :year => "1950").should_not be_valid
+		end
+
+		it "paper without title shouldn't be valid" do
+			Paper.new(:title => "computing machinery and intelligence", :year => "1950").should_not be_valid
 		end
 	end
 end
